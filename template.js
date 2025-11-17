f23a893e5c187ca474e1cec2b98f3a4d47de854e
ahmedtamer20-cm <ahmedtamerpro20@gmail.com>
2025-11-18 00:28:04 +0200

    add

--- template.js
+++ template.js
@@ -10,16 +10,16 @@ let emailValid =  document.querySelector("[name='email']")
          }
     }
     // scroll to top
-  let btn = document.querySelector(".button")
+  let scrollToTopBtn = document.querySelector(".button")
 window.onscroll = function(){
     
     if (window.scrollY >= 5000){
-      btn.classList.add("show")
+      scrollToTopBtn.classList.add("show")
     }else{
-      btn.classList.remove("show")
+      scrollToTopBtn.classList.remove("show")
     }
 };
-btn.onclick= function(){
+scrollToTopBtn.onclick= function(){
    
     window.scrollTo({
       top: 0,
@@ -28,6 +28,62 @@ btn.onclick= function(){
     });
 };
 
+// Seconds & Minutes Timers
+let secondsSpan = document.querySelector(".seconds");
+let minutesSpan = document.querySelector(".minutes");
+let secondsCounter = setInterval(secondsCountdown, 1000);
+
+function secondsCountdown() {
+    // قراءة القيمة الحالية للثواني
+    let seconds = parseInt(secondsSpan.innerHTML) || 0;
+
+    // زيادة ثانية
+    seconds += 1;
+
+    // لو عدّينا 59 نرجع 0 ونزوّد الدقيقة
+    if (seconds >= 60) {
+        seconds = 0;
+
+        // تحديث الدقايق
+        let minutes = parseInt(minutesSpan.innerHTML) || 0;
+        minutes += 1;
+
+        // لو عدّينا 59 دقيقة نرجع تاني 0
+        if (minutes >= 60) {
+            minutes = 0;
+        }
+
+        minutesSpan.innerHTML = minutes;
+    }
+
+    secondsSpan.innerHTML = seconds;
+}
+
+// Hours Timer &Days
+let hoursSpan = document.querySelector(".hours");
+let daysSpan = document.querySelector(".days");
+let hoursSpanCounter = setInterval(hoursCountdown,  3600000);
+
+function hoursCountdown() {
+    let hours = parseInt(hoursSpan.innerHTML) || 0;
+
+    // تحديث القيمة حسب الاتجاه الحالي
+    hours += 1;
+
+   
+
+    // لو وصلنا لـ 0 نخلي الاتجاه طالع
+    if (hours >= 24) {
+        hours = 0;
+
+        let days = parseInt(daysSpan.innerHTML) || 0;
+        days += 1;
+
+        daysSpan.innerHTML = days;
+    }
+
+    hoursSpan.innerHTML = hours;
+}
 
 
 
